b98c217ba0a30f748f74755ff844341bbff0f69e
Administrator <Administrator@USER-PC>
2021-03-08 22:49:47 +0900

    11강

---
+++ BBS/src/bbs/BbsDAO.java
@@ -0,0 +1,104 @@
+package bbs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class BbsDAO {
+    private Connection conn;            // DB에 접근하는 객체    // 
+    private ResultSet rs;                // DB data를 담을 수 있는 객체  (Ctrl + shift + 'o') -> auto import
+    
+    public BbsDAO(){ 
+        try {
+            String dbURL = "jdbc:mysql://localhost:3306/bbs";
+            String dbID = "root";
+            String dbPassword = "root";
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }   
+    }
+    public String getDate() {
+    	String SQL = "SELECT NOW()";
+    	try {
+    		PreparedStatement pstmt = conn.prepareStatement(SQL);
+    		rs = pstmt.executeQuery();
+    		if(rs.next()) {
+    			return rs.getString(1);
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return ""; // db오류
+    }
+    public int getNext() {
+    	String SQL = "SELECT bbsID FROM BBS ORDER BY bbsID DESC";
+    	try {
+    		PreparedStatement pstmt = conn.prepareStatement(SQL);
+    		rs = pstmt.executeQuery();
+    		if(rs.next()) {
+    			return rs.getInt(1) + 1;
+    		}
+    		return 1; // 첫번째 게시물
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return -1;// db오류
+    }
+    public int write(String bbsTitle, String userID, String bbsContent) {
+    	String SQL = "INSERT INTO BBS VALUES (?, ?, ?, ?, ?, ?)";
+    	try {
+    		PreparedStatement pstmt = conn.prepareStatement(SQL);
+    		pstmt.setInt(1, getNext());
+    		pstmt.setString(2, bbsTitle);
+    		pstmt.setString(3, userID);
+    		pstmt.setString(4, getDate());
+    		pstmt.setString(5, bbsContent);
+    		pstmt.setInt(6, 1);
+    		return pstmt.executeUpdate();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return -1;// db오류
+    }
+    public ArrayList<Bbs> getList(int pageNumber) {
+    	String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+    	ArrayList<Bbs> list = new ArrayList<Bbs>();
+    	try {
+    		PreparedStatement pstmt = conn.prepareStatement(SQL);
+    		pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+    		rs = pstmt.executeQuery();
+    		while (rs.next()) {
+    			Bbs bbs = new Bbs();
+    			bbs.setBbsID(rs.getInt(1));
+    			bbs.setBbsTitle(rs.getString(2));
+    			bbs.setUserID(rs.getString(3));
+    			bbs.setBbsDate(rs.getString(4));
+    			bbs.setBbsContent(rs.getString(5));
+    			bbs.setBbsAvailable(rs.getInt(6));
+    			list.add(bbs);
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return list;// db오류
+    }
+    public boolean nextPage(int pageNumber) {
+    	String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1";
+    	try {
+    		PreparedStatement pstmt = conn.prepareStatement(SQL);
+    		pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+    		rs = pstmt.executeQuery();
+    		if(rs.next()) {
+    			return true;
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return false;// db오류 
+    }
+}
